90d0cc62e32f0ad70ef6f118788dd4d6b840930c
genome3po <jeffrey.rubi.885@my.csun.edu>
2017-12-09 14:41:50 -0800

    Initial commit

---
+++ Arrays and Strings/Question1_1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Created by Jeprox on 12/9/17.
+ * Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data
+ * structures?
+ */
+
+public class Question1_1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter any string: ");
+        String problem = input.next();
+
+        System.out.println("Is the string unique? " + isUnique(problem));
+
+    }
+
+    public static boolean isUnique(String unique) {
+
+        for(int i = 0; i < unique.length(); i++) {
+
+            for(int j = i+1; j < unique.length(); j++) {
+                if (unique.charAt(i) == unique.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
---
+++ Arrays and Strings/Question1_2.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Jeprox on 12/9/17.
+ * Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+ */
+
+public class Question1_2 {
+}

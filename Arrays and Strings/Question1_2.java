aa340ef914a5844857b12d34ed247ff16d81e9e6
genome3po <jeffrey.rubi.885@my.csun.edu>
2017-12-09 21:31:47 -0800

    Completed store strings in HashMap. Completed comparisons. Solved Problem

--- Arrays and Strings/Question1_2.java
+++ Arrays and Strings/Question1_2.java
@@ -1,4 +1,7 @@
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Jeprox on 12/9/17.
@@ -19,7 +22,53 @@ public class Question1_2 {
     }
 
     public static boolean isPermutation(String string1, String string2) {
-        return true;
+        // first lets check if both strings are equal length.
+        if(string1.length() != string2.length()) return false;
+        else {
+            // check if both are permutation of each other.
+            // first store both strings in a HashMap.
+            Map<String, Integer> map1;
+            Map<String, Integer> map2;
+
+            map1 = storeInHM(string1);
+            map2 = storeInHM(string2);
+
+            System.out.println("Map 1 looks like this: " + map1);
+            System.out.println("Map 2 looks like this: " + map2);
+
+            // Get all entries into a set
+            Set<Map.Entry<String, Integer>> entrySet = map1.entrySet();
+
+            // Compare all entries from each entry to map2
+            for(Map.Entry<String, Integer> entry : entrySet) {
+
+                String key = entry.getKey();
+                if (!map2.containsKey(key) || entry.getValue() != map2.get(key)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static Map<String, Integer> storeInHM(String toBeStored) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < toBeStored.length(); i++) {
+
+            String key = String.valueOf(toBeStored.charAt(i));
+
+            if(!map.containsKey(key)) map.put(key, 1);
+
+            else {
+                int value = map.get(key);
+                value++;
+                map.put(key, value);
+            }
+        }
+
+        return map;
     }
 
 }

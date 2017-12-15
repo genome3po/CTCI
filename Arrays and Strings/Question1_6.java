a49c070b9fe65990761c62c5f340d61ab39957b6
genome3po <jeffrey.rubi.885@my.csun.edu>
2017-12-14 16:19:48 -0800

    Problem 1_6 Added debugging statements.

--- Arrays and Strings/Question1_6.java
+++ Arrays and Strings/Question1_6.java
@@ -9,10 +9,23 @@
 
 public class Question1_6 {
     public static void main(String[] args) {
-
+        String r = compress("aabcccccaa");
+        System.out.println("This is the final print: " + r);
     }
 
     public static String compress(String s) {
+        // Let's make the string more workable by creating a char array
+        char[] c = s.toCharArray();
+        StringBuilder b = new StringBuilder();
+
+        System.out.print("Printing while in the method: ");
+        for(char c1: c) {
+            System.out.print(c1);
+            b.append(c1);
+        }
+        System.out.print("\n");
+
+        return String.valueOf(b);
 
     }
 
